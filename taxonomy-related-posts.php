a897cf4eada4ea2111fe682e5c3391e8e372301e
Stefano Dotta <stefano@twoswisshikers.net>
2014-11-02 18:56:03 -0600

    Re-added post type selection in widget

--- taxonomy-related-posts.php
+++ taxonomy-related-posts.php
@@ -12,6 +12,7 @@
         function __construct() {
           
 		$this->defaults = array(
+			'title' => __( 'Related Posts' , 'taxonomy-related-posts'),
 			'display_taxonomy_name' => false,
 			'taxonomy'	=> 'post_tag',
 			'post_type'	=> 'post',
@@ -54,20 +55,20 @@
 		
                ?>
                
-               <p>
+		<p>
                 <label for="<?php echo $this->get_field_id( 'title' ); ?>">
-                <?php _e( 'Title' , 'taxonomy-related-posts'); ?>: <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo attribute_escape( $title ); ?>" />
+                <?php _e( 'Title:' , 'taxonomy-related-posts'); ?><input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo attribute_escape( $title ); ?>" />
 		</label>
-               </p>
-               
-               <p>
-		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('display_taxonomy_name'); ?>" name="<?php echo $this->get_field_name('display_taxonomy_name'); ?>" value="1" <?php checked( $instance['display_taxonomy_name'] ); ?> />
-		<label for="<?php echo $this->get_field_id('display_taxonomy_name'); ?>"><?php _e( 'Display taxonomy name as subtitle', 'taxonomy-related-posts' ); ?></label>
-		</p>	
+		</p>
                
 		<p>
+		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('display_taxonomy_name'); ?>" name="<?php echo $this->get_field_name('display_taxonomy_name'); ?>" value="1" <?php checked( $instance['display_taxonomy_name'] ); ?> />
+		<label for="<?php echo $this->get_field_id('display_taxonomy_name'); ?>"><?php _e( 'Display taxonomy name as subtitle.', 'taxonomy-related-posts' ); ?></label>
+		</p>	
+                              
+		<p>
 		<label for="<?php echo $this->get_field_id( 'taxonomy' ); ?>">
-                <?php _e( 'Related by taxonomy' , 'taxonomy-related-posts'); ?>: <select class="widefat" id="<?php echo $this->get_field_id( 'taxonomy' ); ?>" name="<?php echo $this->get_field_name( 'taxonomy' ); ?>"><?php
+                <?php _e( 'Taxonomy to use:' , 'taxonomy-related-posts'); ?><select class="widefat" id="<?php echo $this->get_field_id( 'taxonomy' ); ?>" name="<?php echo $this->get_field_name( 'taxonomy' ); ?>"><?php
                         $taxonomies = get_taxonomies( array( 'show_ui' => true ), 'objects' );
                         foreach ( $taxonomies as $slug => $tax ): ?>
                         	<option value="<?php echo $slug; ?>" <?php echo ( $slug == $taxonomy ) ? 'selected="selected"' : ''; ?>><?php echo $tax->labels->name; ?></option><?php
@@ -77,27 +78,39 @@
 		</p>
                
 		<p>
-		<label for="<?php echo $this->get_field_id( 'order' ); ?>"><?php _e( 'Sorting order:', 'taxonomy-related-posts' ); ?></label> 
+		<label for="<?php echo $this->get_field_id( 'post_type' ); ?>">
+		<?php _e( 'Post type to use:' , 'taxonomy-related-posts'); ?><select class="widefat" id="<?php echo $this->get_field_id( 'post_type' ); ?>" name="<?php echo $this->get_field_name( 'post_type' ); ?>"><?php
+			$post_types = get_post_types( array( 'show_ui' => true, 'public' => true ), 'objects', 'and' );
+			foreach ( $post_types as $slug => $pt ): ?>
+				<option value="<?php echo $slug; ?>" <?php echo ( $slug == $post_type ) ? 'selected="selected"' : ''; ?>><?php echo $pt->labels->name; ?></option><?php
+			endforeach; ?>
+			</select>
+		</label>
+		</p>
+               
+		<p>
+		<label for="<?php echo $this->get_field_id( 'order' ); ?>"><?php _e( 'List sorting order:', 'taxonomy-related-posts' ); ?></label> 
 		<select class="widefat" id="<?php echo $this->get_field_id( 'order' ); ?>" name="<?php echo $this->get_field_name( 'order' ); ?>">
 			<?php foreach ( $order as $option_value => $option_label ) { ?>
 				<option value="<?php echo esc_attr( $option_value ); ?>" <?php selected( $instance['order'], $option_value ); ?>><?php echo esc_html( $option_label ); ?></option>
 			<?php } ?>
 		</select>
 		</p>
-		
-		<p>
-                <label for="<?php echo $this->get_field_id( 'related_count' ); ?>"></label> 
-                    <?php _e( 'Number of posts to show (use -1 to show all posts)' , 'taxonomy-related-posts'); ?>: <input class="widefat" id="<?php echo $this->get_field_id( 'related_count' ); ?>" name="<?php echo $this->get_field_name( 'related_count' ); ?>" type="text" value="<?php echo attribute_escape( $related_count ); ?>" />
-		</p>
                
 		<p>
-		<label for="<?php echo $this->get_field_id( 'list_type' ); ?>"><?php _e( 'List format:', 'taxonomy-related-posts' ); ?></label> 
+		<label for="<?php echo $this->get_field_id( 'list_type' ); ?>"><?php _e( 'List format for display:', 'taxonomy-related-posts' ); ?></label> 
 		<select class="widefat" id="<?php echo $this->get_field_id( 'list_type' ); ?>" name="<?php echo $this->get_field_name( 'list_type' ); ?>">
 			<?php foreach ( $list_type as $option_value => $option_label ) { ?>
 				<option value="<?php echo esc_attr( $option_value ); ?>" <?php selected( $instance['list_type'], $option_value ); ?>><?php echo esc_html( $option_label ); ?></option>
 			<?php } ?>
 		</select>
 		</p>
+
+		<p>
+                <label for="<?php echo $this->get_field_id( 'related_count' ); ?>"></label> 
+		<?php _e( 'Number of posts to show (use -1 to show all posts):' , 'taxonomy-related-posts'); ?><input class="widefat" id="<?php echo $this->get_field_id( 'related_count' ); ?>" name="		<?php echo $this->get_field_name( 'related_count' ); ?>" type="text" value="<?php echo attribute_escape( $related_count ); ?>" />
+		</p>
+
                               
                <?php
 
@@ -108,7 +121,7 @@
 
 		$instance = $old_instance;
 		$instance['title'] = $new_instance['title'];
-		// $instance['post_type'] = $new_instance['post_type'];
+		$instance['post_type'] = $new_instance['post_type'];
 		$instance['order'] = strip_tags( $new_instance['order'] );
 		$instance['related_count'] = $new_instance['related_count'];
 		$instance['taxonomy'] = $new_instance['taxonomy'];
@@ -129,7 +142,7 @@
                
 		$instance = wp_parse_args( (array) $instance, $this->defaults ); 
              
-		// $post_type = $instance['post_type'];
+		$post_type = $instance['post_type'];
 		$display_taxonomy_name = $instance['display_taxonomy_name'];
 		$related_count = $instance['related_count'];
 		$order = $instance['order'];
@@ -174,7 +187,7 @@
 		$query = array(
 			'posts_per_page' => $related_count,
 			'order'          => $order,
-			'post_type' 	 => 'post',
+			'post_type' 	 => $post_type,
 			'tax_query' => array(
 				array(
 					'taxonomy' => $taxonomy,
